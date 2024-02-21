5c6cf7caa3e272fc06b35433a756b5fc444c5ad4
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-02-21 14:25:21 -0600

    Migrations added.

---
+++ MyResume/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MyResume.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyResume.Context.DbResumeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MyResume.Context.DbResumeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
