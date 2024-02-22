19b83e15c10785961a35e4cfdb0dc454f1f3ed97
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-02-21 21:23:13 -0600

    Created controller.

---
+++ MyResume/Controllers/ExperienceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyResume.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
