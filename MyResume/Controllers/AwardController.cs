99798fb0587f2aaf5d319db278e83852fe9995eb
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-02-23 14:08:49 -0600

    Template added. Controller created.

---
+++ MyResume/Controllers/AwardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyResume.Controllers
+{
+    public class AwardController : Controller
+    {
+        // GET: Award
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
